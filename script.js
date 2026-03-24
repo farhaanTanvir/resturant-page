76c0f4521699d2744d505fe8b3620d8e2d2ef4c4
Farhan Tanvir <ftanvir723@gmail.com>
2026-03-24 11:16:52 +0600

    Complete!

--- script.js
+++ script.js
@@ -57,3 +57,47 @@ drinks.addEventListener('click', (e) => {
 })
 
 
+
+
+
+// HAMBURGER MENU
+
+const hamButton = document.querySelector('.ham');
+const filter = document.querySelector('.dark-filter');
+const header = document.querySelector('header');
+
+
+
+function hideHam() {
+    const root = document.documentElement;
+    root.style.setProperty('--right', '-100%');
+    document.removeEventListener('click', insideHam);
+    // filter.classList.remove('filter-active');
+    // header.classList.remove('header-active');
+}
+
+
+
+function showHam(event) {
+    const root = document.documentElement;
+    root.style.setProperty('--right', '0%');
+    document.addEventListener('click', insideHam);
+    event.stopPropagation();
+    // filter.classList.add('filter-active');
+    // header.classList.add('header-active');
+}
+
+function insideHam(event) {
+    hideHam();
+    if (event.target.classList.contains("ham-item")) {
+
+    } else {
+        event.preventDefault();
+    }
+};
+
+
+hamButton.addEventListener('click', showHam);
+
+// HAMBURGER MENU
+
